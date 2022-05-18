41836d997feaea7bd5be9d0fbc3ec392aa5c6f99
Jongbin Lim <rkwk0515@gmail.com>
2022-05-18 14:54:24 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <div className="mainlogo">
+      <img src={logo} className="App-logo" alt="logo"  />
+      </div>
+      <div className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          jbini React
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
