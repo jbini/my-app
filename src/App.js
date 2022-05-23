b95adf46d543711204200a671850d62d7b688a81
Jongbin Lim <rkwk0515@gmail.com>
2022-05-23 18:39:45 +0900

    0523

--- src/App.js
+++ src/App.js
@@ -37,24 +37,38 @@ function App() {
             <input type="radio" name='slide1' id='firstslide01' checked></input>
             <input type="radio" name='slide1' id='firstslide02'></input>
             <input type="radio" name='slide1' id='firstslide03'></input>
-            <div>
-              <ul>
+            <div className='slidewrap1'>
+              <ul className='slidelist1'>
                 <li>
                   <a href='http://localhost:3000/'>
-                    <img src='./src/img/slide01.jpg'></img>
+                    <img src='/src/img/slide01.jpg' alt='slide1'></img>
                   </a>
                 </li>
                 <li>
                   <a href='http://localhost:3000/'>
-                    <img src='./img/slide02.jpg'></img>
+                    <img src='/src/img/slide02.jpg' alt='slide2'></img>
                   </a>
                 </li>
                 <li>
                   <a href='http://localhost:3000/'>
-                    <img src='./img/slide03.jpg'></img>
+                    <img src='/src/img/slide03.jpg' alt='slide3'></img>
                   </a>
                 </li>
               </ul>
+              <div className='first-slide-control'>
+                <div className='control01'>
+                  <label for='firstslide03' className='slide-left'></label>
+                  <label for='firstslide02' className='slide-right'></label>
+                </div>
+                <div className='control02'>
+                  <label for='firstslide01' className='slide-left'></label>
+                  <label for='firstslide03' className='slide-right'></label>
+                </div>
+                <div className='control03'>
+                  <label for='firstslide02' className='slide-left'></label>
+                  <label for='firstslide01' className='slide-right'></label>
+                </div>
+              </div>
             </div>
           </div>
           first
