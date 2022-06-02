a924be44a85cc3ab5af190ab3644b329e8163f39
Jongbin Lim <rkwk0515@gmail.com>
2022-06-02 16:21:37 +0900

    0602

--- src/App.js
+++ src/App.js
@@ -155,12 +155,17 @@ function App() {
           github : <a href='https://github.com/jbini' target='_blank' rel="noreferrer">https://github.com/jbini</a>  
         </div>
         <div className='footer-div-menu1'>
-          <p>Categories</p>
-          <a>1</a><a>2</a><a>3</a>
+          <h2>Menu1</h2>
+          <a href='https://github.com/jbini' target='_blank' rel="noreferrer">About link1</a>
+          <p></p><a href='https://github.com/jbini' target='_blank' rel="noreferrer">For link2</a>
+          <p></p><a href='https://github.com/jbini' target='_blank' rel="noreferrer">Where is link3</a>
+          <p></p><a href='https://github.com/jbini' target='_blank' rel="noreferrer">What is link4</a>
         </div>
         <div className='footer-div-menu2'>
-          <p>Categories</p>
-          <a>1</a><a>2</a><a>3</a>
+          <h2>Menu2</h2>
+          <a href='https://github.com/jbini' target='_blank' rel="noreferrer">Is that link5</a>
+          <p></p><a href='https://github.com/jbini' target='_blank' rel="noreferrer">Yes link6</a>
+          <p></p><a href='https://github.com/jbini' target='_blank' rel="noreferrer">Thanks link7</a>
         </div>
         <div className='footer-div-icon'>
           <a href='https://www.facebook.com' target='_blank' rel="noreferrer">
