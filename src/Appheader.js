09b739f0595f2b7862913dd3f1d70a3abe7d9c02
Jongbin Lim <rkwk0515@gmail.com>
2022-06-05 20:12:17 +0900

    0605

---
+++ src/Appheader.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './Appheader.css';
+
+export default function AppHeaders() {
+    return (
+        <div className="App-header">
+            <div className='logo-flex'>
+            <img src={logo} className="App-logo" alt="logo"  />
+            <a
+                className="App-link"
+                href="http://localhost:3000/"
+                rel="noopener noreferrer"
+            >
+                jbini React
+            </a>
+            </div>
+            <div className='nav-container'>
+            <ul>
+                <li><a href="http://localhost:3000/" className='nav-a'>Home</a></li>
+                <li> <a href="http://localhost:3000/" className='nav-a'>Categories</a>
+                    <ul>
+                        <li><a href='CategoryOne.js'>Category One</a></li>
+                        <li><a href="http://localhost:3000/">Category Two</a></li>
+                        <li><a href="http://localhost:3000/">Category Three</a></li>
+                    </ul>
+                </li>
+                <li><a href="https://reactjs.org" className='nav-a' target='_blank' rel="noreferrer">Learn React</a></li>
+            </ul>
+            </div>
+        </div>
+      );
+}
