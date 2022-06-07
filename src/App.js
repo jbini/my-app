e6a2a870d69cd64c56a9523c446eac7420e59c3b
Jongbin Lim <rkwk0515@gmail.com>
2022-06-07 14:51:19 +0900

    0607

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import AppHeaders from './component/Appheader';
 import FirstContainer from './component/FirstContainer';
 import Footer from './component/Footer';
 import SecondContainer from './component/SecondContainer';
-
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 export default function App() {
   return (
-    <div className="App" >
-      <AppHeaders />
-      <FirstContainer />
-      <div className='line-container'>
-        <div className='grayline'></div>
+    <BrowserRouter>
+      <div className="App" >
+        <AppHeaders />
+        
+          <FirstContainer />
+          <div className='line-container'>
+            <div className='grayline'></div>
+          </div>
+          <SecondContainer />
+        
+        <Footer />
       </div>
-      <SecondContainer />
-      
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
     
 }
