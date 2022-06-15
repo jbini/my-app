7f4ad76d4eb2e8548f043a297d537d086efdf118
Jongbin Lim <rkwk0515@gmail.com>
2022-06-15 16:41:24 +0900

    0615

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,15 @@ import SecondContainer from './component/SecondContainer';
 import GrayLine from './component/GrayLine';
 import ThirdContainer from './component/ThirdContainer';
 import Banner from './component/Banner';
+import FilterList from './component/FilterList';
 
 export default function Home() {
   
   return (
       <div>
         <Banner />
+        <GrayLine />
+        <FilterList />
         <GrayLine />
         <ThirdContainer />
         <GrayLine />
---
+++ src/component/FilterList.js
@@ -0,0 +1,28 @@
+import '../css/FilterList.css'
+
+export default function FilterList() {
+    return(
+        <div className='filterlist-container'>
+            <div className='filterlist-filter-div'>
+                <div className='filterlist-filter'>
+                    <a>a</a>
+                </div>
+                <div className='filterlist-filter'>
+                    <a>a</a>
+                </div>
+                <div className='filterlist-filter'>
+                    <a>a</a>
+                </div>
+                <div className='filterlist-filter'>
+                    <a>a</a>
+                </div>
+                <div className='filterlist-filter'>
+                    <a>a</a>
+                </div>
+            </div>
+            <div className='filterlist-div'>
+
+            </div>
+        </div>
+    );
+}
