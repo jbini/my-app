99d63ee6da6af1ce22af26e8e23469915cd67662
Jongbin Lim <rkwk0515@gmail.com>
2022-06-15 17:32:21 +0900

    0615

--- src/component/FilterList.js
+++ src/component/FilterList.js
@@ -21,7 +21,83 @@ export default function FilterList() {
                 </div>
             </div>
             <div className='filterlist-div'>
-
+                <div className='filterlist-list1'>
+                    <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test message test message test message test message</p>
+                    </div>
+                </div>
+                <div className='filterlist-list'>
+                <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test</p>
+                    </div>
+                </div>
+                <div className='filterlist-list'>
+                <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test message test message test message test message test message test message test message test message</p>
+                    </div>
+                </div>
+                <div className='filterlist-list'>
+                    <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test</p>
+                    </div>
+                </div>
+                <div className='filterlist-list1'>
+                <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test test message test message test message test message</p>
+                    </div>
+                </div>
+                <div className='filterlist-list'>
+                <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test test message test test message</p>
+                    </div>
+                </div>
+                <div className='filterlist-list'>
+                <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test </p>
+                    </div>
+                </div>
+                <div className='filterlist-list'>
+                <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test test message test message test message test message message message message message message message</p>
+                    </div>
+                </div>
+                <div className='filterlist-list1'>
+                <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test</p>
+                    </div>
+                </div>
+                <div className='filterlist-list'>
+                <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test message message</p>
+                    </div>
+                </div>
+                <div className='filterlist-list'>
+                <div className='filterlist-list-img-div'></div>
+                    <div className='filterlist-list-sub-div'>
+                        <h2>This is Title</h2>
+                        <p>test test test test test test</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
