cf74fadc31d1442c45607f06b5afed0e9c1ee861
Jongbin Lim <rkwk0515@gmail.com>
2022-06-17 22:28:17 +0900

    0617

--- src/component/FilterList.js
+++ src/component/FilterList.js
@@ -4,24 +4,27 @@ import { Link } from 'react-router-dom';
 export default function FilterList() {
     return(
         <div className='filterlist-container'>
-            <div className='filterlist-filter-div'>
-                <div className='filterlist-filter-check'>
-                    <Link to='/'>Gray</Link>
+            <div className='filterlist-sec'>
+                <div className='filterlist-filter-div'>
+
+                    <div className='filterlist-filter-check'>
+                        <Link to='/'>Gray</Link>
+                    </div>
+                    <div className='filterlist-filter'>
+                        <Link to='/blue'>Blue</Link>
+                    </div>
+                    <div className='filterlist-filter'>
+                        <Link to='/red'>Red</Link>
+                    </div>
+                    <div className='filterlist-filter'>
+                        <Link to='/yellow'>Yellow</Link>
+                    </div>
+                    <div className='filterlist-filter'>
+                        <Link to='/green'>Green</Link>
+                    </div>
                 </div>
-                <div className='filterlist-filter'>
-                    <Link to='/blue'>Blue</Link>
-                </div>
-                <div className='filterlist-filter'>
-                    <Link to='/red'>Red</Link>
-                </div>
-                <div className='filterlist-filter'>
-                    <Link to='/yellow'>Yellow</Link>
-                </div>
-                <div className='filterlist-filter'>
-                    <Link to='/green'>Green</Link>
-                </div>
-            </div>
-            <div className='filterlist-div'>
+
+                <div className='filterlist-div'>
                 <div className='filterlist-list1'>
                     <div className='filterlist-list-img-div-gray'></div>
                     <div className='filterlist-list-sub-div'>
@@ -71,7 +74,12 @@ export default function FilterList() {
                         <p>test test message test message test message test message message message message message message message</p>
                     </div>
                 </div>
+                
             </div>
+
+            </div>
+            
+            
         </div>
     );
 }
