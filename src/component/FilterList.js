8db881198da1549696c9bb34b5cb0e880c3d5c1e
Jongbin Lim <rkwk0515@gmail.com>
2022-06-21 18:44:02 +0900

    0621

--- src/component/FilterList.js
+++ src/component/FilterList.js
@@ -1,10 +1,20 @@
 import '../css/FilterList.css'
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import FilterListGrayList1 from './FilterListGrayList1';
+import FilterListGrayList from './FilterListGrayList';
 
 export default function FilterList() {
 
-    let [listTitle,listTitleChange] = useState(['This is Title','This is Title2']);
+    // const title1 = 'This is Title1';
+    // const title2 = 'This is Title2';
+    // const title3 = 'This is Title3';
+    // const title4 = 'This is Title4';
+    // const title5 = 'This is Title5';
+    // const title6 = 'This is Title6';
+    // const title7 = 'This is Title7';
+    // const title8 = 'This is Title8';
+    let [listTitle,listTitleChange] = useState([]);
     
     return(
         <div className='filterlist-container'>
@@ -31,70 +41,15 @@ export default function FilterList() {
 
                 <div className='filterlist-div'>
 
+                    <FilterListGrayList1 listNumber={1} subContents={'test message1 test message1 test message1 test message1'} />
+                    <FilterListGrayList listNumber={2} subContents={'test message2 test message2 test message2 test message2'} />
+                    <FilterListGrayList listNumber={3} subContents={'test message3 test message3 test message3 test message3'} />
+                    <FilterListGrayList listNumber={4} subContents={'test message4 test message4 test message4 test message4'} />
+                    <FilterListGrayList1 listNumber={5} subContents={'test message5 test message5 test message5 test message5'} />
+                    <FilterListGrayList listNumber={6} subContents={'test message6 test message6 test message6 test message6'} />
+                    <FilterListGrayList listNumber={7} subContents={'test message7 test message7 test message7 test message7'} />
+                    <FilterListGrayList listNumber={8} subContents={'test message8 test message8 test message8 test message8'} />
                     
-                    <input type='checkbox' id='graylist1' name='listcheck' />
-                    <label className='dark-bg' for='graylist1' />
-                    <div className='filterlist-large-div'>
-                        <div className='filterlist-large-top'>
-                            <label for='graylist1'><h2>X</h2></label>
-                        </div>
-                        <div className='filterlist-large-img' />
-                        <div className='filterlist-large-sub'>
-                            <h2>{listTitle[0]}</h2>
-                            <p>test message test message test message test message</p>
-                        </div>
-                    </div>
-
-                    <label className='filterlist-list1' for='graylist1'>
-                        <div className='filterlist-list-img-div-gray'></div>
-                        <div className='filterlist-list-sub-div'>
-                            <h2>This is Title</h2>
-                            <p>test message test message test message test message</p>
-                        </div>
-                    </label>
-                    <label className='filterlist-list' for='graylist1'>
-                    <div className='filterlist-list-img-div-gray'></div>
-                        <div className='filterlist-list-sub-div'>
-                            <h2>This is Title</h2>
-                            <p>test</p>
-                        </div>
-                    </label>
-                    <label className='filterlist-list' for='graylist1'>
-                    <div className='filterlist-list-img-div-gray'></div>
-                        <div className='filterlist-list-sub-div'>
-                            <h2>This is Title</h2>
-                            <p>test message test message test message test message test message test message test message test message</p>
-                        </div>
-                    </label>
-                    <label className='filterlist-list' for='graylist1'>
-                        <div className='filterlist-list-img-div-gray'></div>
-                        <div className='filterlist-list-sub-div'>
-                            <h2>This is Title</h2>
-                            <p>test</p>
-                        </div>
-                    </label>
-                    <label className='filterlist-list1' for='graylist1'>
-                    <div className='filterlist-list-img-div-gray'></div>
-                        <div className='filterlist-list-sub-div'>
-                            <h2>This is Title</h2>
-                            <p>test test message test message test message test message</p>
-                        </div>
-                    </label>
-                    <label className='filterlist-list' for='graylist1'>
-                    <div className='filterlist-list-img-div-gray'></div>
-                        <div className='filterlist-list-sub-div'>
-                            <h2>This is Title</h2>
-                            <p>test test message test test message</p>
-                        </div>
-                    </label>
-                    <label className='filterlist-list' for='graylist1'>
-                    <div className='filterlist-list-img-div-gray'></div>
-                        <div className='filterlist-list-sub-div'>
-                            <h2>This is Title</h2>
-                            <p>test test message test message test message test message message message message message message message</p>
-                        </div>
-                    </label>
-                
                 </div>
 
             </div>
---
+++ src/component/FilterListGrayList1.js
@@ -0,0 +1,38 @@
+import '../css/FilterList.css'
+import { useState } from 'react';
+import styled from "styled-components";
+
+export default function FilterListGrayList1(props) {
+    
+    let setNumber = props.listNumber;
+
+    return (
+        
+        <div className='filterlist-list-div'>
+
+            <input type='checkbox' id={'graylist'+setNumber} name='listcheck' />
+            <label className='dark-bg' for={'graylist'+setNumber} />
+
+            <div className='filterlist-large-div'>
+                <div className='filterlist-large-top'>
+                    <label for={'graylist'+setNumber}><h2>X</h2></label>
+                </div>
+                <div className='filterlist-large-img' />
+                <div className='filterlist-large-sub'>
+                    <h2>This is Title {props.listNumber}</h2>
+                    <p>{props.subContents}</p>
+                </div>
+            </div>
+
+            <label className='filterlist-list1' for={'graylist'+setNumber}>
+                <div className='filterlist-list-img-div-gray'></div>
+                <div className='filterlist-list-sub-div'>
+                    <h2>This is Title {props.listNumber}</h2>
+                    <p>{props.subContents}</p>
+                </div>
+            </label>
+
+        </div>
+        
+    );
+}
